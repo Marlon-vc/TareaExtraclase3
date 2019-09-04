12775c7fe4310726e0cebeff7e8385c1b7414975
Marlon Vega <mvegach2013@gmail.com>
2019-09-04 17:35:00 -0600

    Thread code

--- src/Gui.cpp
+++ src/Gui.cpp
@@ -2,8 +2,13 @@
 #include <gtkmm-3.0/gtkmm/window.h>
 #include <gtkmm-3.0/gtkmm/button.h>
 #include <gtkmm-3.0/gtkmm/fixed.h>
+#include <gtkmm-3.0/gtkmm/box.h>
+#include <gtkmm-3.0/gtkmm/image.h>
 #include <gdkmm-3.0/gdkmm/pixbuf.h>
+#include <thread>
 #include <iostream>
+
+typedef void *(*THREADFUNCPTR)(void *);
 
 class Game: public Gtk::Window {
     public:
@@ -11,25 +16,60 @@ class Game: public Gtk::Window {
         virtual ~Game();
     private:
         Gtk::Button button1;
-        Gtk::Fixed container;
-        void pressed();
+        Gtk::Box container;
+        Gtk::Fixed plane;
+        Gtk::Image cat;
+
+        void move();
+        void startThread();
 };
 
-Game::Game(): button1("Hello") {
+Game::Game(): button1("start"), container(Gtk::ORIENTATION_VERTICAL){
 
+    // container.put();
+    // container.move();
+
+    Glib::RefPtr<Gdk::Pixbuf> pix = Gdk::Pixbuf::create_from_file("/home/marlon/TEC/II Semestre 2019/Datos II/TareaExtraclase3/res/cat.png", 20, 20);
+    cat = Gtk::Image(pix);
+
+    plane.put(cat, 250, 150);
+    plane.show_all_children();
+
+    button1.signal_clicked().connect(sigc::mem_fun(*this, &Game::startThread));
+
+    container.add(plane);
+    container.add(button1);
     add(container);
 
-    set_size_request(500, 300);
+    set_size_request(500, 500);
     set_title("Pathfinding Game");
     show_all_children();
+    
 }
 
 Game::~Game() {
 
 }
+void Game::startThread() {
+    // pthread_t thread;
+    // int rc;
+    // rc = pthread_create(&thread, NULL, (THREADFUNCPTR) &Game::move, this);
+    // if (rc) {
+    //     std::cout << "Unable to start thread..\n";
+    // }
+    std::thread my_thread(move);
+    
+}
 
-void Game::pressed() {
-    std::cout << "Pressed\n";
+void Game::move() {
+    std::cout << "Moving\n";
+    int x = 0;
+    int y = 0;
+    while (true) {
+        plane.move(cat, x, y);
+        x++;
+        y++;
+    }
 }
 
 int main(int argc, char *argv[]) {
